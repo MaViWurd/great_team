32ace939d6a8c690ccd9a4b918d5e71a31004007
MaViWurd <marvelousegbe17@gmail.com>
2022-07-29 23:08:41 +0100

    comments

---
+++ basics/8comment.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	/* 
+	Multi line Comment
+	Comment Here 
+	*/
+	//Single line comment i.e can be added anywhere including the end of a statement or command
+	//TODO
+	//Explain lines of code for a complex line of code
+	
+	printf("Comments are fun!\n");
+
+	//printf("Comments are fun!\n"); comment out a line of code cause its been commented out instead of deleting it
+	
+	//Comment are way that we can write out information in our files that won't get rendered by C-Program
+
+	return 0;
+}
