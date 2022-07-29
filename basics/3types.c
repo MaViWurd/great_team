ec8b4852b7792c498b69f4c5b0f7ba977707a13d
MaViWurd <marvelousegbe17@gmail.com>
2022-07-29 21:35:57 +0100

    data type

---
+++ basics/3types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int age = 40; //An interger
+	double gpa = 3.7; //Used to represent decimal
+	char grade = 'A'; //Storing single character in C, NB: you can't store more than a single character
+
+	char phrase[] = "Hello World!"; //string: collection of charater that are grouped together
+
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+
+	return 0;
+}
